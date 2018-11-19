66770eadcacfe3d64c36cf9417cd09745ae71eaf
haakensonb <untamedhawk44@gmail.com>
2018-11-19 18:27:46 -0500

    vertices are connected properly in gridGraph

--- Graph.java
+++ Graph.java
@@ -12,40 +12,99 @@ public class Graph {
     public void buildGridGraph(int numOfRows, int numOfCols){
         int totalVertices = numOfRows * numOfCols;
         this.vertices = new HashMap<>();
+        int currentRow = 0;
         for(int i=0; i < totalVertices; i++){
+
+            // increment the current row
+            if (((i % numOfCols) == 0) && (i != 0)){
+                currentRow += 1; 
+            }
+
             String vertexId = "V" + i;
             Vertex vertex = new Vertex();
 
-            // top left corner
-            if(i == 0){
+            int topLeftCornerIndex = 0;
+            int topRightCornerIndex =  numOfCols-1;
+            int bottomLeftCornerIndex = totalVertices-numOfCols;
+            int bottomRightCornerIndex = totalVertices-1;
+            int leftColumnIndex = (currentRow * numOfCols);
+            int rightColumnIndex = (currentRow * numOfCols) + (numOfCols - 1);
+
+            if(i == topLeftCornerIndex){
                 // vertex to right of top left corner
                 vertex.addEdge(vertexId, "V1");
                 // vertex under top left corner
                 vertex.addEdge(vertexId, ("V"+numOfCols));
+ 
+            } else if (i == topRightCornerIndex){
+                // vertex before
+                vertex.addEdge(vertexId, ("V"+(i-1)));
+                // vertex under
+                vertex.addEdge(vertexId, ("V"+(i+numOfCols)));
 
-            // middle of top row
-            } else if(i > 0 && i < (numOfCols-1)){
+            } else if (i == bottomLeftCornerIndex){
+                // vertex above
+                vertex.addEdge(vertexId, ("V"+(i-numOfCols)));
+                // vertex to right
+                vertex.addEdge(vertexId, ("V"+(i+1)));
+
+            } else if (i == bottomRightCornerIndex){
+                // vertex above
+                vertex.addEdge(vertexId, ("V"+(i-numOfCols))); 
+                // vertex to left
+                vertex.addEdge(vertexId, ("V"+(i-1)));
+
+            } else if ((i > topLeftCornerIndex) && (i < topRightCornerIndex)){
                 // vertex before
                 vertex.addEdge(vertexId, ("V"+(i-1)));
                 //vertex after
                 vertex.addEdge(vertexId, ("V"+(i+1)));
                 //vertex below
-                vertex.addEdge(vertexId, ("V"+(i+numOfCols)));
+                vertex.addEdge(vertexId, ("V"+(i+numOfCols))); 
 
-            // top right corner
-            } else if(i == numOfCols-1){
-                // vertex before
+            } else if (i == leftColumnIndex){
+                // vertex above
+                vertex.addEdge(vertexId, ("V"+(i-numOfCols)));
+                // vertex to right
+                vertex.addEdge(vertexId, ("V"+(i+1)));
+                // vertex below
+                vertex.addEdge(vertexId, ("V"+(i+numOfCols))); 
+
+            } else if (i == rightColumnIndex){
+                // vertex above
+                vertex.addEdge(vertexId, ("V"+(i-numOfCols)));
+                // vertex to left
                 vertex.addEdge(vertexId, ("V"+(i-1)));
-                // vertex under
+                // vertex below 
                 vertex.addEdge(vertexId, ("V"+(i+numOfCols)));
-            }
 
+            } else if ((i > bottomLeftCornerIndex) && (i < bottomRightCornerIndex)){
+                // vertex above
+                vertex.addEdge(vertexId, ("V"+(i-numOfCols)));
+                // vertex to left
+                vertex.addEdge(vertexId, ("V"+(i-1)));
+                // vertex to right
+                vertex.addEdge(vertexId, ("V"+(i+1))); 
+
+            } else {
+                // vertex above
+                vertex.addEdge(vertexId, ("V"+(i-numOfCols)));
+                // vertex to left
+                vertex.addEdge(vertexId, ("V"+(i-1)));
+                // vertex to right
+                vertex.addEdge(vertexId, ("V"+(i+1)));
+                // vertex below 
+                vertex.addEdge(vertexId, ("V"+(i+numOfCols))); 
+            } // end if/else chain
+
+            // now that proper edges have been placed put the vertex in the hashmap
             this.vertices.put(vertexId, vertex);
-        }
+
+        } // end for loop
     } // end buildGridGraph
 
     public static void main(String[] args){
-        Graph g = new Graph(3, 3);
+        Graph g = new Graph(4, 3);
 
         // iterate through the vertices using for each loop
         for(Map.Entry<String, Vertex> vertex : g.vertices.entrySet()){
